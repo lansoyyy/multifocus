c660bb3a15663fec7a12ecfab443f110b8d08f44
Lans12345 <olanalans12345@gmail.com>
2023-06-28 09:25:05 +0800

    added initial routes and color palletes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:multifocus/screens/home_page.dart';
+import 'package:multifocus/screens/home_screen.dart';
+import 'package:multifocus/utils/colors.dart';
+import 'package:multifocus/utils/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      color: primary,
       title: 'MultiFocus',
-      home: HomePage(),
+      home: const HomeScreen(),
+      routes: {
+        Routes().homescreen: (context) => const HomeScreen(),
+      },
     );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
 
+const primary = Color(0xff161921);
+const secondary = Color(0xff47505E);
+const primaryGrey = Color(0xff7C848C);
+const secondaryGrey = Color(0xffACB4BC);
+const white = Color(0xffFCFCFC);
